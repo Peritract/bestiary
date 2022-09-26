c287a7b85d149d6a268f420cbf7ba943531c3eb3
Peritract <dan@getfutureproof.co.uk>
2022-09-26 11:36:19 +0100

    Commit correct starting files

---
+++ server/beasts.js
@@ -0,0 +1,13 @@
+const beasts = [
+    { id: 0, name: "Cyclops", habitat: "Greek islands", dangerRating: 3,
+      description: "A one-eyed giant", encounterRate: 0.05,
+      loot: ["a giant eye", "sheep", "blood"] },
+    { id: 1, name: "Siren", habitat: "Greek islands", dangerRating: 5,
+      description: "Really good looking, but slightly psychotic", encounterRate: 0.5,
+      loot: ["a haunting melody", "sailors' bones"] },
+    { id: 2, name: "Minotaur", habitat: "Greek labyrinths", dangerRating: 4,
+      description: "Half-man, half-bull", encounterRate: 0.3,
+      loot: ["six pack", "horns", "thread"] },
+];
+
+module.exports = beasts;
